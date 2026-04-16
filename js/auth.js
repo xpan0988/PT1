29a88fbd51a240ed57f96ed48b9a25fcb7e7f03e
xpan0988 <xiyuanpan86@gmail.com>
2026-04-16 22:37:32 +1000

    fixing auth file

--- js/auth.js
+++ js/auth.js
@@ -238,6 +238,7 @@
       clearGroupModalError();
 
       updateHeaderGroupTag();
+      switchView('dashboard');
       document.getElementById('groupModal').classList.remove('open');
       showAuthUI();
     }
